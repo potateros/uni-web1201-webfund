8f484dd307c19985f53a4d2c1811f54fd3b6d2d8
Eric Ngu <ericngu97@gmail.com>
2017-11-01 15:07:03 +0800

    added audio

--- index.js
+++ index.js
@@ -21,33 +21,17 @@ carousel();
 function carousel() {
     var i;
     var x = document.getElementsByClassName("slides");
-    
+
     for (i = 0; i < x.length; i++) {
-        x[i].style.display = "none"; 
+        x[i].style.display = "none";
     }
-    
+
     slideIndex++;
-    
+
     if (slideIndex > x.length) {
         slideIndex = 1;
     }
-    
-    x[slideIndex-1].style.display = "block"; 
+
+    x[slideIndex-1].style.display = "block";
     setTimeout(carousel, 2000); // Change image every 2 seconds
 }
-
-/* $(function() {
-    $('#gif-hover').hide();
-    $('#hover').on("mouseover", function() {
-      $('#hover').hide();
-      $('#gif-hover').show();
-      $('#pic-hover').hide();
-    });
-
-    $('#gif-hover').on("mouseout", function() {
-      $('#hover').show();
-      $('#gif-hover').hide();
-      $('#pic-hover').show();
-    });
-});
- */
